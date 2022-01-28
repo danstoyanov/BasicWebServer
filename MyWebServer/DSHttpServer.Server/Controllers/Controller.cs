c3f88d586d104c80a1ef0db285a1bdd1f9f664ab
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-01-28 17:13:10 +0200

    ✔ Step 2: Create Controller Class !

---
+++ MyWebServer/DSHttpServer.Server/Controllers/Controller.cs
@@ -0,0 +1,31 @@
+﻿
+
+using DSHttpServer.Server.HTTP;
+using DSHttpServer.Server.Responses;
+
+namespace DSHttpServer.Server.Controllers
+{
+    public abstract class Controller
+    {
+        public Controller(Request request)
+        {
+            this.Request = request;
+        }
+
+        protected Request Request { get; private init; }
+
+        protected Response Text(string text) => new TextResponse(text);
+
+        protected Response Html(string text) => new HtmlResponse(text);
+
+        protected Response BadRequest() => new BadRequestResponse();
+
+        protected Response Unauthoriazed() => new UnauthorizedResponse();
+
+        protected Response NotFount() => new NotFoundResponse();
+
+        protected Response Redirect(string location) => new RedirectResponse(location);
+
+        protected Response File(string fileName) => new TextFileResponse(fileName);
+    }
+}
