b63e330849aedc15a89feded186c25227d5de2d7
Daniel Stoyanov <dan.stoyanov@abv.bg>
2022-01-28 17:43:34 +0200

    Step 4.1 HomeController Class !

---
+++ MyWebServer/DSHttpServer.Demo/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using DSHttpServer.Server.Controllers;
+using DSHttpServer.Server.HTTP;
+
+namespace DSHttpServer.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(Request request) 
+            : base(request)
+        {
+
+        }
+    }
+}
